31b0751abb579a1b3c2e6a2e830d832ac1bcdaf9
Firehose <firehose@firehose-VirtualBox.(none)>
2013-11-16 12:05:40 -0500

    Initial Commit; Built a rails application; added a splash page; it looks beautiful

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firehose::Application.config.secret_key_base = '9e4593a38f464b74e35491fc7165d4de1594a27016e6243ed7d950a46f852700bfacea8358f4240d009d39b76b4b0fe9074127b993d9d1fb1bc546e1f6bcd9c2'
