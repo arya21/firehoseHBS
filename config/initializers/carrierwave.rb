579d10857a77cafaea981f752c8e89b2b71184ab
Firehose <firehose@firehose-VirtualBox.(none)>
2013-11-17 12:00:10 -0500

    Changing images to be stored on Amazon S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  
+  if Rails.env.production?   #if on heroku environment (production)
+
+      config.fog_credentials = {
+        :provider               => 'AWS',                      # required
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
+        :aws_secret_access_key  => ENV['AWS_SECRET_KEY']      # required
+        
+      }
+      
+      config.fog_directory  = ENV['AWS_BUCKET']                # required
+   
+      config.storage = :fog
+
+  else
+  
+      config.storage = :file
+
+  end    
+
+
+end
